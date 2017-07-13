60a13ef0e03163bad5f3d4410fd9e8fc66fe88c8
ressiver <diospyros.digyna@gmail.com>
2017-07-13 13:23:21 +0300

    done first function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-﻿
+﻿'use strict'
+
+function areIntersected(rectangleFirst, rectangleSecond) {
+	return (rectangleFirst.top < rectangleSecond.top || rectangleFirst.top > rectangleSecond.top || rectangleFirst.height < rectangleSecond.height || rectangleFirst.height >= rectangleSecond.height)
+}
+
+function filterVisible({ }, []) {
+
+}
