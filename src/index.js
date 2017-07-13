6769b87925c85d48a0b52541f8575d5b82514ced
ressiver <diospyros.digyna@gmail.com>
2017-07-13 16:25:34 +0300

    done all fuctions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 ﻿'use strict'
 
-function areIntersected(rectangleFirst, rectangleSecond) {
-	return (rectangleFirst.top < rectangleSecond.top || rectangleFirst.top > rectangleSecond.top || rectangleFirst.height < rectangleSecond.height || rectangleFirst.height >= rectangleSecond.height)
+function areIntersected(r1, r2) {
+	if (r1.left >= (r2.left + r2.width) || (r1.left + r1.width) <= r2.left || r1.top > (r2.top + r2.height) || (r1.top + r1.height) < r2.top) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
-function filterVisible({ }, []) {
-
+function filterVisible(r1, array) {
+	return array.filter(function (value) {
+		if (value.width !== 0 && value.height !== 0) {
+			return areIntersected(r1, value);
+		} else {
+			return false;
+		}
+	});
 }
